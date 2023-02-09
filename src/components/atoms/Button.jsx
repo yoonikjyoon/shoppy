1e3f4c17c7adb8f1d792e875d8716aee346dfddb
Yooni <yooni.kj.yoon@gmail.com>
2023-02-09 14:21:14 +0900

    add height porps in button component

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Button({ text, onClick, type = "button" }) {
+export default function Button({
+  text,
+  onClick,
+  type = "button",
+  height = 35,
+}) {
   return (
-    <Container type={type} onClick={onClick}>
+    <Container type={type} onClick={onClick} height={height}>
       {text}
     </Container>
   );
 }
 const Container = styled.button`
+  height: ${(props) => props.height && `${props.height}px`};
   padding: 5px 10px;
   border-radius: 5px;
   background-color: var(--lavender-color);
