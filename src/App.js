a5a704dad4597e4c01cf15bfc7c01664d2823c1c
Yooni <yooni.kj.yoon@gmail.com>
2023-02-10 18:53:55 +0900

    add react-query

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import Header from "./components/organisms/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/organisms/Footer";
 import { AuthContextProvider } from "./components/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Header />
-      <Outlet />
-      <Footer />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Header />
+        <Outlet />
+        <Footer />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
