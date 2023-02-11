1f657a95a5a744133fdabbdfe2cf40dde9ecf195
Yooni <yooni.kj.yoon@gmail.com>
2023-02-11 15:38:10 +0900

    styled HomeBanner component

--- src/components/molecules/HomeBanner.jsx
+++ src/components/molecules/HomeBanner.jsx
@@ -9,7 +9,10 @@ export default function HomeBanner() {
         src={require("../../assets/bgImage.JPG")}
         alt="background image in home banner"
       />
-      {/* <BackgroundContainer /> */}
+      <BackgroundContainer>
+        <h2>Shop With Us</h2>
+        <p>Best Products, High Quality</p>
+      </BackgroundContainer>
       <StyledPadding height={20} />
     </Container>
   );
@@ -17,6 +20,7 @@ export default function HomeBanner() {
 const Container = styled.div`
   width: 100%;
   height: auto;
+  position: relative;
 `;
 const BackgroundImage = styled.img`
   width: 100%;
@@ -24,10 +28,11 @@ const BackgroundImage = styled.img`
   object-fit: cover;
   object-position: 50% 60%;
 `;
-// const BackgroundContainer = styled.div`
-//   width: 100%;
-//   height: 400px;
-//   background-image: url("../../assets/bgImage.JPG");
-//   background-position: center;
-//   background-size: cover;
-// `;
+const BackgroundContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -75%);
+  text-align: center;
+  color: var(--white-color);
+`;
