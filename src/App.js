4e084c860c5825994079249ebf8ef1e3093542a4
Yooni <yooni.kj.yoon@gmail.com>
2023-03-22 14:52:19 +0900

    Add scroll top feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import { useEffect } from "react";
 import Header from "./components/organisms/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./components/organisms/Footer";
 import { AuthContextProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -7,6 +8,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
