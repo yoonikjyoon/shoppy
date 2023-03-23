1c27d1b3fd375f5f920b2088141cbed44249c199
Yooni <yooni.kj.yoon@gmail.com>
2023-03-23 18:20:04 +0900

    Fix bug

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import useProducts from "../../hooks/useProducts";
 import HomeBanner from "../molecules/HomeBanner";
 import Products from "../organisms/Products";
 
 export default function Home() {
+  const [lastKey, setLastKey] = useState(null);
+  const [itemList, setItemList] = useState([]);
+  const {
+    productsQuery: { isLoading, error, data: products },
+  } = useProducts("", lastKey);
+
+  useEffect(() => {
+    if (products) {
+      const items = products.itemList;
+      setItemList([...itemList, ...items]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [products]);
+
+  const handleLoadMore = () => {
+    setLastKey(products.lastKey);
+  };
   return (
     <Container>
       <HomeBanner />
-      <ProductContainer>{/* <Products /> */}</ProductContainer>
+      <ProductContainer>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {products && (
+          <Products
+            itemList={itemList}
+            hasNextPage={products.hasNextPage}
+            handleLoadMore={handleLoadMore}
+          />
+        )}
+      </ProductContainer>
     </Container>
   );
 }
